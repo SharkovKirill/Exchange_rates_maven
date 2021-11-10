57187d34afc43c1aaf4a356b12b5dcb94d640290
SharkovKirill <kirill.office75@gmail.com>
2021-11-10 17:13:23 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+//import com.mashape.unirest.http.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        for (; ;) {
+//            API.getIDbyCurrency("20/10/2021","USD");
+        System.out.println("\nДля выхода введите exit\n");
+        System.out.println("Введите дату в формате dd/MM/yyyy, например 20/10/2021: ");
+        Scanner input = new Scanner(System.in);
+        String date = input.next();
+        if(date.equals("exit")){
+            input.close();
+            break;
+        }
+        System.out.println("Введите идентификатор валюты, например USD: ");
+        String currency = input.next();
+        if(currency.equals("exit")){
+            input.close();
+            break;
+        }
+        API.getResponse(date, currency);
+        }
+
+
+    }
+}
